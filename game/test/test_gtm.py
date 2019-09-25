0ed9acb34b5102e49ee511c8313fb81a5e583d6b
StevenLaptop <steven_supit@hotmail.com>
2019-09-25 17:50:41 +0200

    Added test environment

---
+++ game/test/test_gtm.py
@@ -0,0 +1,17 @@
+"""
+Test for Gathering the Magic
+"""
+
+import unittest
+from game.player import Player
+
+
+class TestGTM(unittest.TestCase):
+    """Class to hold all testing logic"""
+    def setUp(self):
+        """Overrides the setUp fixture of the superclass"""
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
